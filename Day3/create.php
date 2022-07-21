dcdb8ad25e6c9e55be92f85fb50cc4386af6d4a3
AnhJenda <100700718+TinyTots-ePj@users.noreply.github.com>
2022-07-21 23:07:47 +0700

    update

--- Day3/create.php
+++ Day3/create.php
@@ -1,69 +1,76 @@
 <?php
-// include config file
+
 require_once 'config.php';
-// define variables and initialize with empty values
+
+//Khai báo các biến rỗng
 $name = $address = $salary = "";
-$name_err = $salary_err = $address_arr = "";
-// processing form data when form is submitted
-if ($_SERVER["REQUEST_METHOD"]=="POST"){
-    // validate name
+$name_err = $address_err = $salary_err = "";
+
+// Lấy dữ liệu từ form
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    ;
+
+//Validate name
     $input_name = trim($_POST["name"]);
-    if (empty($input_name)){
-        $name_err = "Please enter a name";
-    } elseif (!filter_var(trim($_POST["name"]), FILTER_VALIDATE_REGEXP, array("options" => array("regexp"=>"/^[a-zA-Z'-.\s]+$/")))){
-        $name_err = "Please enter a valid name";
-    } else{
+    if (empty($input_name)) {
+        $name_err = "Please enter a name.";
+    } elseif (!filter_var(trim($_POST["name"]), FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^[a-zA-Z'-.\s ]+$")))) {
+        $name_err = 'Please enter valid name.';
+    } else {
         $name = $input_name;
     }
-    // validate address
+
+//Validate address
     $input_address = trim($_POST["address"]);
-    if (empty($input_address)){
-        $address_arr = "Please enter address";
-    } else{
+    if (!empty($input_address)) {
+        $address_err = 'Please enter a address.';
+    } else {
         $address = $input_address;
     }
-    // validate salary
+
+//Validate salary
     $input_salary = trim($_POST["salary"]);
-    if (empty($input_salary)){
-        $salary_err = "Please enter the salary amount";
-    }elseif (!ctype_digit($input_salary)){
-        $salary_err = "Please enter the positive integer value";
-    } else{
+    if (!empty($input_salary)) {
+        $salary_err = "Please enter a salary.";
+    } elseif (!ctype_digit($input_salary)) {
+        $salary_err = 'Please enter a positive integer value';
+    } else {
         $salary = $input_salary;
     }
-    // check input error before insert to database
-    if (empty($name_err) && empty($address_arr) && empty($salary_err)){
-        // prepare an insert statement
+
+//Kiểm tra lỗi sau khi nhập vào database
+    if (empty($name_err) && empty($address_err) && empty($salary_err)) {
+        //Câu lệnh prepare insert
         $sql = "INSERT INTO employees(name, address, salary) VALUES (?, ?, ?)";
-        if ($stmt = mysqli_prepare($link, $sql)){
-            // bind variables to the prepare statement parameter
-            mysqli_stmt_bind_param($stmt, "sss", $param_name, $param_address, $param_salary);
-            // set parameter
+
+        if ($stmt = mysqli_prepare($link, $sql)) {
+            mysqli_stmt_bind_param($stmt, "sssi", $param_name, $param_address, $param_salary);
+
+            //Truyền vào các kiểu dữ liệu
             $param_name = $name;
             $param_address = $address;
             $param_salary = $salary;
-            // attempt to execute prepare statement
-            if (mysqli_stmt_execute($stmt)){
-                //Records created successfully. redirect to landing page
-                header("index.php");
+
+            if (mysqli_stmt_prepare($stmt)) {
+                header("location: index.php");
                 exit();
-            }else{
-                echo "Something went wrong. please try again later";
+            } else {
+                echo "Something went wrong. Please try again later.";
             }
-        }mysqli_stmt_close($stmt);
-    }mysqli_close($link);
+        }
+        mysqli_stmt_close($stmt);
+    }
+    mysqli_close($link);
 }
 ?>
-<!doctype html>
+
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn/bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <meta charset = "UTF-8">
     <title>Create Record</title>
-    <style type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style>
         .wrapper{
             width: 500px;
             margin: 0 auto;
@@ -71,36 +78,36 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
     </style>
 </head>
 <body>
-    <div class="wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="page-header">
-                        <h2>Create Record</h2>
-                    </div>
-                    <p>Please fill the form and submit to add the employee to database</p>
-                    <form action="<?php echo htmlspecialchars($_SERVER(["PHP_SELF"]));?>" method="post">
-                        <div class="form-group <?php echo(!empty($name_err)) ? 'has-error' : ''; ?>">
-                            <label>Name</label>
-                            <input type="text" name="name" class="form-control" value="<?php echo $name ;?>">
-                            <span class="help-block"><?php echo $name_err;?></span>
-                        </div>
-                        <div class="form-group <?php echo(!empty($address_err)) ? 'has-error' : ''; ?>">
-                            <label>Address</label>
-                            <textarea name="address" class="form-control"><?php echo $address; ?></textarea>
-                            <span class="help-block"><?php echo $address_err;?></span>
-                        </div>
-                        <div class="form-group <?php echo(!empty($salary_err)) ? 'has-error' : ''; ?>">
-                            <label>Salary</label>
-                            <input type="text" name="salary" class="form-control" value="<?php echo $salary; ?>">
-                            <span class="help-block"><?php echo $salary_err;?></span>
-                        </div>
-                        <input type="submit" class="btn btn-primary" value="Submit">
-                        <a href="index.php" class="btn btn-default"> Cancel </a>
-                    </form>
+<div class="wrapper">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="page-header">
+                    <h2>Create Record</h2>
                 </div>
+                <p>Please fill in this form and submit to add employee record to database.</p>
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+                    <div class="form-group <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
+                        <label>Name</label>
+                        <input type="text" name="name" class="form-control" value="<?php echo $name; ?>">
+                        <span class="help-block"><?php echo $name_err; ?></span>
+                    </div>
+                    <div class="form-group <?php echo (!empty($address_err)) ? 'has-error' : ''; ?>">
+                        <label>Name</label>
+                        <input type="text" name="name" class="form-control" value="<?php echo $address; ?>">
+                        <span class="help-block"><?php echo $address_err; ?></span>
+                    </div>
+                    <div class="form-group <?php echo (!empty($salary_err)) ? 'has-error' : ''; ?>">
+                        <label>Name</label>
+                        <input type="text" name="name" class="form-control" value="<?php echo $salary; ?>">
+                        <span class="help-block"><?php echo $salary_err; ?></span>
+                    </div>
+                    <input type="submit" class="btn btn-primary" value="Submit">
+                    <a href="index.php" class="btn btn-primary">Cancel</a>
+                </form>
             </div>
         </div>
     </div>
+</div>
 </body>
 </html>
