8bc43a1a3da5e57bb376e1d91bb24b8f21012f68
AnhJenda <100700718+TinyTots-ePj@users.noreply.github.com>
2022-07-29 10:14:03 +0700

    update

---
+++ Day4/UploadImages/gallerry/database.sql
@@ -0,0 +1,7 @@
+create table gallerry (
+    idGallery int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    titleGallery LONGTEXT not null ,
+    descGallery LONGTEXT not null ,
+    imgFullnameGallery LONGTEXT NOT NULL,
+    orderGallery int(11) not null
+);
